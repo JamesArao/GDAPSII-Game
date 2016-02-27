56fef8570bde5a3efc5c9c2943403c4c64884efe
Kiernan Brown (RIT Student) <kwb6721@ad.rit.edu>
2016-02-27 18:39:52 -0500

    Created the basic framework (movement and creation of enemies) for our game. Also created some classes for the game

---
+++ GroupGame/GroupGame/Character1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GroupGame
+{
+    class Character1:Character
+    {
+
+        // Constructor
+        public Character1(int x, int y):base(x,y)
+        {
+            Position = new Rectangle(x, y, 50, 50);
+            Speed = 2;
+        }
+
+        // Override Shoot method
+        public override void Shoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
